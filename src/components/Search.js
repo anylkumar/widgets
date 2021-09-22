dafaf587ca8d8d7b4dc88ef5488273b0a144bc2e
anylkumar <anylkumary@gmail.com>
2021-09-22 10:19:10 +0530

    2 files modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+    search();
+  }, [term]);
 
   const onInputChange = (event) => {
     setTerm(event.target.value);
@@ -12,12 +28,7 @@ const Search = () => {
       <div className="ui form">
         <div className="field">
           <label htmlFor="text">Enter Search Term</label>
-          <input
-            onChange={this.onInputChange}
-            value={term}
-            id="text"
-            type="text"
-          />
+          <input onChange={this.onInputChange} id="text" type="text" />
         </div>
       </div>
     </div>
