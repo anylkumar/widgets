aa153752b4f1db12ad4a7defd1970ae404133008
anylkumar <anylkumary@gmail.com>
2021-09-25 08:12:49 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -36,9 +38,28 @@ const options = [
 ];
 
 export default function App() {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search items={items} />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate items={items} />
+      </Route>
     </div>
   );
 }
